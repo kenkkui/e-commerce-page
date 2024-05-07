4d4a40a8d84f7efb876e592e9a12b4fa63b6f9b8
kenkkui <kenzlain@gmail.com>
2024-05-07 10:56:09 +0800

    i believe im done now :<

--- src/component/Navbar/ShoppingCartContent.tsx
+++ src/component/Navbar/ShoppingCartContent.tsx
@@ -3,7 +3,6 @@ import { CartContent } from "../../types/types";
 import thumbnailImage from "../../images/image-product-1-thumbnail.jpg";
 import RenderImage from "../RenderImage";
 import iconClear from "../../images/icon-delete.svg";
-import { BooleanLiteral } from "typescript";
 
 interface ShoppingCartContentProps {
   isHover: boolean;
@@ -30,7 +29,9 @@ function ShoppingCartContent({
 
   return (
     <section
-      className={`shopping-cart-container ${isHover ? "is-hover" : ""}`}
+      className={`shopping-cart-container ${mobile ? "mobile" : ""} ${
+        isHover ? "is-hover" : ""
+      }`}
       onMouseOver={handleMouseOver}
       onMouseOut={handleMouseOut}
     >
@@ -43,17 +44,19 @@ function ShoppingCartContent({
           {cart > 0 ? (
             <div className="item-in-cart-wrapper">
               <div className="item-in-cart">
-                <RenderImage
-                  src={thumbnailImage}
-                  name="thumbnail-image"
-                  alt="Thumbnail"
-                />
-                <div className="text-container">
-                  <h3>Fall Limited Edition Sneakers</h3>
-                  <p>
-                    $125.00 x {cart}
-                    <p className="cart-total-price">${cart * 125}.00</p>
-                  </p>
+                <div className="main">
+                  <RenderImage
+                    src={thumbnailImage}
+                    name="thumbnail-image"
+                    alt="Thumbnail"
+                  />
+                  <div className="text-container">
+                    <h3>Fall Limited Edition Sneakers</h3>
+                    <p>
+                      $125.00 x {cart}
+                      <p className="cart-total-price">${cart * 125}.00</p>
+                    </p>
+                  </div>
                 </div>
 
                 <button className="clear-cart" onClick={() => setCart(0)}>
